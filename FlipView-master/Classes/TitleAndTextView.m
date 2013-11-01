2e4a6c361144aab1ed68a5a0146f72df268ce875
saisandeep2104 <kavurusai242@gmail.com>
2013-11-01 09:08:13 -0600

    copyright.

--- FlipView-master/Classes/TitleAndTextView.m
+++ FlipView-master/Classes/TitleAndTextView.m
@@ -1,4 +1,32 @@
-
+/*
+ - This module is licensed under the MIT license.
+ -
+ - Copyright (C) 2011 by raw engineering
+ -
+ - Permission is hereby granted, free of charge, to any person obtaining a copy
+ - of this software and associated documentation files (the "Software"), to deal
+ - in the Software without restriction, including without limitation the rights
+ - to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ - copies of the Software, and to permit persons to whom the Software is
+ - furnished to do so, subject to the following conditions:
+ -
+ - The above copyright notice and this permission notice shall be included in
+ - all copies or substantial portions of the Software.
+ -
+ - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ - IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ - FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ - AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ - LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ - OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ - THE SOFTWARE.
+ -
+-//
+-//  TitleAndTextView.m
+-//  FlipView
+-//
+-//  Created by Reefaq Mohammed on 16/07/11.
+ */
 
 #import "TitleAndTextView.h"
 #import "MessageModel.h"
@@ -26,15 +54,11 @@
 	
 	CGSize contentViewArea = CGSizeMake((contentView.frame.size.width - 20), (contentView.frame.size.height-30));
 	
-	//[userNameLabel sizeToFit];
-	//[userNameLabel setFrame:CGRectMake(userImageView.frame.origin.x + userImageView.frame.size.width + 10, 5, (contentViewArea.width - (userImageView.frame.size.width + 10)), userNameLabel.frame.size.height)];
+	[userNameLabel sizeToFit];
 	[timeStampLabel sizeToFit];
-   // [userNameLabel setFrame:CGRectMake(someImageView.frame.origin.x + someImageView.frame.size.width + 10, 5, (contentViewArea.width - (someImageView.frame.size.width + 10)), userNameLabel.frame.size.height)];
+    [userNameLabel setFrame:CGRectMake(someImageView.frame.origin.x + someImageView.frame.size.width + 10, 5, (contentViewArea.width - (someImageView.frame.size.width + 10)), userNameLabel.frame.size.height)];
 	[timeStampLabel sizeToFit];
-	//[timeStampLabel setFrame:CGRectMake(userNameLabel.frame.origin.x, userNameLabel.frame.origin.y + userNameLabel.frame.size.height, timeStampLabel.frame.size.width, timeStampLabel.frame.size.height)];
-
-	
-	//[messageLabel setFrame:CGRectMake(userImageView.frame.origin.x ,(userImageView.frame.origin.y + userImageView.frame.size.height), contentViewArea.width, contentViewArea.height - (userImageView.frame.origin.y + userImageView.frame.size.height))];
+	[timeStampLabel setFrame:CGRectMake(userNameLabel.frame.origin.x, userNameLabel.frame.origin.y + userNameLabel.frame.size.height, timeStampLabel.frame.size.width, timeStampLabel.frame.size.height)];
     [messageLabel setFrame:CGRectMake(someImageView.frame.origin.x ,(someImageView.frame.origin.y + someImageView.frame.size.height), contentViewArea.width, contentViewArea.height - (someImageView.frame.origin.y + someImageView.frame.size.height))];
     
 	[messageLabel setText:messageModel.content];
@@ -58,25 +82,21 @@
     contentView = [[UIView alloc] init];
 	[contentView setBackgroundColor:[UIColor whiteColor]];
 	contentView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-/*
-	userImageView = [[UIImageView alloc] init];
-	userImageView.image = [UIImage imageNamed:messageModel.userImage];
-	[userImageView setFrame:CGRectMake(10, 10, 50, 50)];
-	[contentView addSubview:userImageView];
-    */
-    someImageView = [[UIImageView alloc] init];
-	someImageView.image = [UIImage imageNamed:messageModel.someImage
-                           ];
-	[someImageView setFrame:CGRectMake(10, 10, 300, 200)];
+
+/*****  init with image sets background *****/
+    someImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:messageModel.someImage]];
+//	someImageView.image = [UIImage imageNamed:messageModel.someImage];
+//	[someImageView setFrame:CGRectMake(10, 10, talls, wides)];
 	[contentView addSubview:someImageView];
 
-	/*userNameLabel = [[UILabel alloc] init];
-	[userNameLabel setText:[NSString stringWithFormat:@"%@",messageModel.userName]];
+	userNameLabel = [[UILabel alloc] init];
+//  this was displaying a null value
+//	[userNameLabel setText:[NSString stringWithFormat:@"%@",messageModel.userName]];
 	userNameLabel.font =[UIFont fontWithName:@"Helvetica" size:25];
 	[userNameLabel setTextColor:RGBCOLOR(2,90,177)];
 	[userNameLabel setBackgroundColor:[UIColor clearColor]];
 	[contentView addSubview:userNameLabel];
-	*/
+	
 	
 	timeStampLabel = [[UILabel alloc] init];
 	[timeStampLabel setText:messageModel.createdAt];
@@ -115,7 +135,7 @@
 	[contentView release];
 	//[userImageView release];
     [someImageView release];
-	//[userNameLabel release];
+	[userNameLabel release];
 	[timeStampLabel release];
 	[messageLabel release];
 	[super dealloc];
